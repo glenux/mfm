afb97e0a895fec0af34494da3514188ba7670d72
Glenn <glenux@glenux.net>
2023-11-02 16:55:29 +0100

    fix: split vagrant provisioning into multiple parts

--- scripts/vagrant-provision/build.sh
+++ scripts/vagrant-provision/build.sh
@@ -9,26 +9,6 @@ HOSTNAME="$(hostname)"
 
 export DEBIAN_FRONTEND=noninteractive
 
-echo "Installing required system packages"
-apt-get update --allow-releaseinfo-change
-apt-get install -y \
-   apt-transport-https \
-   ca-certificates \
-   git \
-   curl \
-   wget \
-   vim \
-   gnupg2 \
-   software-properties-common
-
-echo "Installing recording requirements"
-apt-get install -y \
-   tmux \
-   mdp \
-   bat \
-   asciinema \
-   termtosvg
-
 echo "Installing mfm requirements"
 apt-get install -y \
    fzf \
@@ -38,24 +18,6 @@ apt-get install -y \
    libyaml-dev \
    libpcre3-dev \
    libevent-dev
-
-#!/bin/sh
-
-set -e
-set -u
-
-USER="$(test -d /vagrant && echo "vagrant" || echo "debian")"
-CLUSTERS_DIR=/home/$USER/clusters
-
-# Installation de kompose
-if [ ! -f /usr/local/bin/kompose ]; then
-	DL="$(mktemp)"
-	curl \
-		-L https://github.com/kubernetes/kompose/releases/download/v1.22.0/kompose-linux-amd64 \
-		-o "$DL"
-	chmod +x "$DL"
-	mv "$DL" /usr/local/bin/kompose
-fi
 
 # Installing asdf
 su - "$USER" -c "git config --global advice.detachedHead false"
