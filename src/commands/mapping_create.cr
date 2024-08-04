df89dd3dfa18c6a74309564d1fb73a0d87e257f6
Glenn <glenux@glenux.net>
2024-08-04 22:58:09 +0200

    feat: start implementing MappingCreate

--- src/commands/mapping_create.cr
+++ src/commands/mapping_create.cr
@@ -2,11 +2,15 @@ require "./abstract_command"
 
 module GX::Commands
   class MappingCreate < AbstractCommand
-    def initialize(config : GX::Config) # FIXME
+    def initialize(@config : GX::Config) # FIXME
+      @config.load_from_env
+      @config.load_from_file
+      @file_system_manager = FileSystemManager.new(@config)
     end
 
     def execute
       # FIXME: implement
+      puts "mapping create yo!"
     end
 
     def self.handles_mode
